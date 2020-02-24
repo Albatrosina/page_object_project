26a563fe550735952c30c57a96c5ee17ad92671d
Danil <danilkelembet@gmail.com>
2020-02-24 18:08:23 +0200

    Product page tests were expanded.

--- pages/product_page.py
+++ pages/product_page.py
@@ -15,7 +15,7 @@ class ProductPage(BasePage):
         ).text, "Basket total isn't equal to product price"
 
     def product_name_added_should_be_correct(self):
-        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text in self.browser.find_element(
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text == self.browser.find_element(
            *ProductPageLocators.BASKET_CONFIRMATION_MSG
         ).text, "Product name in the notification isn't fit the product name added"
 
