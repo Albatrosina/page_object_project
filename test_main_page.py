9045ab8d1be0b5fd50ebf0779250137733473500
Danil <danilkelembet@gmail.com>
2020-02-26 18:55:17 +0200

    Final changes were made. Preparing repo to be reviewed

--- test_main_page.py
+++ test_main_page.py
@@ -3,12 +3,13 @@ from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
 
+link = "http://selenium1py.pythonanywhere.com/"
+
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage:
 
     def test_guest_can_go_to_login_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
         # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page = MainPage(browser, link)
         # открываем страницу
@@ -19,14 +20,12 @@ class TestLoginFromMainPage:
         login_page.should_be_login_page()
 
     def test_guest_should_see_login_link(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = BasketPage(browser, link)
     # 1.Гость открывает главную страницу
     page.open()
